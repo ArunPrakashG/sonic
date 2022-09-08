72738c091b47814b899df5faaa835329f2ee8a58
Arun Prakash <arun.prakash.456789@gmail.com>
2022-09-08 22:01:02 +0530

    initial commit

---
+++ lib/sonic.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library sonic;
+
+export 'src/sonic_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
